bfdbf07d2e85e7db89f3c85691166e4f2c7edeea
luciemariec <132121627+luciemariec@users.noreply.github.com>
2024-01-21 21:52:49 -0600

    finished part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,21 @@ let candidateAnswer = " ";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+"Who was the first American woman in space? ", 
+"True or false: 5 kilometer == 5000 meters? ", 
+"(5 + 3)/2 * 10 = ? ", 
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+"What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -23,18 +35,18 @@ candidateName = input.question("Welcome! What is your name? ")
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-console.log("1. " + question);
-candidateAnswer = input.question("Answer: ");
-
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers[i] = input.question(`${i+1}) ${questions[i]}`)
+  console.log(`Your Answer: ${candidateAnswers[i]}`);
+  console.log(`Correct Answer: ${correctAnswers[i]}\n`);
+  }
 }
-
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer===correctAnswer) {
-    console.log("That's right!");
-  } else {
-    console.log("Sorry, that's incorrect.");
+  for (let i = 0; i < candidateAnswers.length; i++) {
+    let answer = candidateAnswers[i];
+    let correct = correctAnswers[i];
   }
 
 
